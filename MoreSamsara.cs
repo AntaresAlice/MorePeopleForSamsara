b59f5b96ddd6d7d77c1bf20f003f7af780d54d69
AntaresAlice <antaresalice@qq.com>
2024-08-02 22:09:53 +0800

    update to fix bug

--- MoreSamsara.cs
+++ MoreSamsara.cs
@@ -1,4 +1,4 @@
-﻿using TaiwuModdingLib.Core.Plugin;
+using TaiwuModdingLib.Core.Plugin;
 using GameData.Domains.Building;
 using GameData.Utilities;
 using HarmonyLib;
@@ -45,87 +45,156 @@ namespace MorePeopleForSamsara
         }
 
 
-        [HarmonyPrefix, HarmonyPatch(
-            declaringType: typeof(BuildingDomain),
-            methodName: "GetSamsaraPlatformCharList")]
-        public static bool BuildingDomain_GetSamsaraPlatformCharList_PrePatch(
+    [HarmonyPrefix]
+    public static bool PrePatch(
         BuildingDomain __instance,
-        ref List<SamsaraPlatformCharDisplayData> __result
-            )
+        DataContext context,
+        bool excludeCharactersInSlot,
+        ref List<SamsaraPlatformCharDisplayData> __result)
+    {
+        if (MoreSamsara.samsaraRangeSetting == 0)
         {
-            if (MoreSamsara.samsaraRangeSetting == 0) {
-                return true;
-            }
-            // gender setting
-            sbyte gender = (sbyte)(MoreSamsara.genderSetting);
-
-            // List<SamsaraPlatformCharDisplayData> dataList = new List<SamsaraPlatformCharDisplayData>();
-            __result = new List<SamsaraPlatformCharDisplayData>();
-            HashSet<int> waitingReincarnationChars = ObjectPool<HashSet<int>>.Instance.Get();
-            DomainManager.Character.GetAllWaitingReincarnationCharIds(waitingReincarnationChars);
-            int currDate = WorldDomain.GetWorldInfo().CurrDate;
-            int durDate = 0;
-            if (deaMonthSetting != 0)
-            {
-                if (deaMonthSetting > currDate)
-                {
-                    durDate = 0;
-                }
-                else
-                {
-                    durDate = currDate - deaMonthSetting;
-                }
-            }
-            foreach (int charId in waitingReincarnationChars)
-            {
-                bool flag = __instance.IsCharOnSamsaraPlatform(charId, false) || ProfessionSkillHandle.BuddhistMonkSkill_IsDirectedSamsaraCharacter(charId);
-                if (!flag)
-                {
-                    DeadCharacter deadChar = DomainManager.Character.GetDeadCharacter(charId);
-
-                    if (gender == 0 || gender == 1)
-                    {
-                        if (deadChar.Gender == gender) continue;
-                    }
-
-                    // int taiwuId = DomainManager.Taiwu.GetTaiwuCharId();
-                    int taiwuSettlementId = DomainManager.Taiwu.GetTaiwuVillageSettlementId();
-                    if (MoreSamsara.samsaraRangeSetting == 2)
-                    {
-                        if (deadChar.OrganizationInfo.SettlementId != taiwuSettlementId) continue;
-                    }
-
-                    if (deaMonthSetting != 0)
-                    {
-                        if (deadChar.DeathDate < durDate) continue;
-                    }
-
-                    if (gradeSetting != 9)
-                    {
-                        if (deadChar.OrganizationInfo.Grade != gradeSetting) continue;
-                    }
-
-                    //AdaptableLog.Info(DomainManager.Character.GetNameRelatedData(charId).GetRealName().surname + DomainManager.Character.GetNameRelatedData(charId).GetRealName().givenName);
-                    //AdaptableLog.Info(deadChar.DeathDate.ToString());
-                    //AdaptableLog.Info(deadChar.OrganizationInfo.SettlementId.ToString());
-                    //AdaptableLog.Info(deadChar.OrganizationInfo.Grade.ToString());
-
-                    SamsaraPlatformCharDisplayData displayData = new SamsaraPlatformCharDisplayData
-                    {
-                        Id = charId,
-                        NameRelatedData = DomainManager.Character.GetNameRelatedData(charId),
-                        AvatarRelatedData = new AvatarRelatedData(deadChar),
-                        MainAttributes = deadChar.BaseMainAttributes,
-                        CombatSkillQualifications = deadChar.BaseCombatSkillQualifications,
-                        LifeSkillQualifications = deadChar.BaseLifeSkillQualifications
-                    };
-                    __result.Add(displayData);
-                }
-                ObjectPool<HashSet<int>>.Instance.Return(waitingReincarnationChars);
-            }
-            // AdaptableLog.Info("More Samsara now.");
-            return false;
+            return true;
         }
+
+        sbyte gender = (sbyte)(MoreSamsara.genderSetting);
+        __result = new List<SamsaraPlatformCharDisplayData>();
+
+        HashSet<int> waitingReincarnationChars = ObjectPool<HashSet<int>>.Instance.Get();
+        DomainManager.Character.GetAllWaitingReincarnationCharIds(waitingReincarnationChars);
+
+        int currDate = WorldDomain.GetWorldInfo().CurrDate;
+        int durDate = currDate - (deaMonthSetting != 0 && deaMonthSetting <= currDate ? deaMonthSetting : 0);
+
+        foreach (int charId in waitingReincarnationChars)
+        {
+            bool isValidChar = __instance.IsCharOnSamsaraPlatform(charId, false) ||
+                               ProfessionSkillHandle.BuddhistMonkSkill_IsDirectedSamsaraCharacter(charId);
+
+            if (!isValidChar)
+            {
+                DeadCharacter deadChar = DomainManager.Character.GetDeadCharacter(charId);
+
+                if ((gender == 0 || gender == 1) && deadChar.Gender != gender)
+                {
+                    continue;
+                }
+
+                int taiwuSettlementId = DomainManager.Taiwu.GetTaiwuVillageSettlementId();
+                if (MoreSamsara.samsaraRangeSetting == 2 && deadChar.OrganizationInfo.SettlementId != taiwuSettlementId)
+                {
+                    continue;
+                }
+
+                if (deaMonthSetting != 0 && deadChar.DeathDate < durDate)
+                {
+                    continue;
+                }
+
+                if (gradeSetting != 9 && deadChar.OrganizationInfo.Grade != gradeSetting)
+                {
+                    continue;
+                }
+
+                SamsaraPlatformCharDisplayData displayData = new SamsaraPlatformCharDisplayData
+                {
+                    Id = charId,
+                    TemplateId = deadChar.TemplateId,
+                    NameRelatedData = DomainManager.Character.GetNameRelatedData(charId),
+                    AvatarRelatedData = new AvatarRelatedData(deadChar),
+                    MainAttributes = deadChar.BaseMainAttributes,
+                    CombatSkillQualifications = deadChar.BaseCombatSkillQualifications,
+                    LifeSkillQualifications = deadChar.BaseLifeSkillQualifications
+                };
+                __result.Add(displayData);
+            }
+        }
+
+        ObjectPool<HashSet<int>>.Instance.Return(waitingReincarnationChars);
+        return false; // Skip the original method
+    }
+
+        // [HarmonyPrefix, HarmonyPatch(
+        //     declaringType: typeof(BuildingDomain),
+        //     methodName: "GetSamsaraPlatformCharList")]
+        // public static bool BuildingDomain_GetSamsaraPlatformCharList_PrePatch(
+        // BuildingDomain __instance,
+        // ref List<SamsaraPlatformCharDisplayData> __result
+        //     )
+        // {
+        //     if (MoreSamsara.samsaraRangeSetting == 0) {
+        //         return true;
+        //     }
+        //     // gender setting
+        //     sbyte gender = (sbyte)(MoreSamsara.genderSetting);
+
+        //     // List<SamsaraPlatformCharDisplayData> dataList = new List<SamsaraPlatformCharDisplayData>();
+        //     __result = new List<SamsaraPlatformCharDisplayData>();
+        //     HashSet<int> waitingReincarnationChars = ObjectPool<HashSet<int>>.Instance.Get();
+        //     DomainManager.Character.GetAllWaitingReincarnationCharIds(waitingReincarnationChars);
+        //     int currDate = WorldDomain.GetWorldInfo().CurrDate;
+        //     int durDate = 0;
+        //     if (deaMonthSetting != 0)
+        //     {
+        //         if (deaMonthSetting > currDate)
+        //         {
+        //             durDate = 0;
+        //         }
+        //         else
+        //         {
+        //             durDate = currDate - deaMonthSetting;
+        //         }
+        //     }
+        //     foreach (int charId in waitingReincarnationChars)
+        //     {
+        //         bool flag = __instance.IsCharOnSamsaraPlatform(charId, false) || ProfessionSkillHandle.BuddhistMonkSkill_IsDirectedSamsaraCharacter(charId);
+        //         if (!flag)
+        //         {
+        //             DeadCharacter deadChar = DomainManager.Character.GetDeadCharacter(charId);
+
+        //             if (gender == 0 || gender == 1)
+        //             {
+        //                 if (deadChar.Gender == gender) continue;
+        //             }
+
+        //             // int taiwuId = DomainManager.Taiwu.GetTaiwuCharId();
+        //             int taiwuSettlementId = DomainManager.Taiwu.GetTaiwuVillageSettlementId();
+        //             if (MoreSamsara.samsaraRangeSetting == 2)
+        //             {
+        //                 if (deadChar.OrganizationInfo.SettlementId != taiwuSettlementId) continue;
+        //             }
+
+        //             if (deaMonthSetting != 0)
+        //             {
+        //                 if (deadChar.DeathDate < durDate) continue;
+        //             }
+
+        //             if (gradeSetting != 9)
+        //             {
+        //                 if (deadChar.OrganizationInfo.Grade != gradeSetting) continue;
+        //             }
+
+        //             //AdaptableLog.Info(DomainManager.Character.GetNameRelatedData(charId).GetRealName().surname + DomainManager.Character.GetNameRelatedData(charId).GetRealName().givenName);
+        //             //AdaptableLog.Info(deadChar.DeathDate.ToString());
+        //             //AdaptableLog.Info(deadChar.OrganizationInfo.SettlementId.ToString());
+        //             //AdaptableLog.Info(deadChar.OrganizationInfo.Grade.ToString());
+
+        //             SamsaraPlatformCharDisplayData displayData = new SamsaraPlatformCharDisplayData
+        //             {
+        //                 Id = charId,
+        //                 NameRelatedData = DomainManager.Character.GetNameRelatedData(charId),
+        //                 AvatarRelatedData = new AvatarRelatedData(deadChar),
+        //                 MainAttributes = deadChar.BaseMainAttributes,
+        //                 CombatSkillQualifications = deadChar.BaseCombatSkillQualifications,
+        //                 LifeSkillQualifications = deadChar.BaseLifeSkillQualifications
+        //             };
+        //             __result.Add(displayData);
+        //         }
+        //         ObjectPool<HashSet<int>>.Instance.Return(waitingReincarnationChars);
+        //     }
+        //     // AdaptableLog.Info("More Samsara now.");
+        //     return false;
+        // }
 
         private static int genderSetting;
         private static int samsaraRangeSetting;
